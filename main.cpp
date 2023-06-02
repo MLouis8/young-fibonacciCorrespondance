25ad47f0089fac1ac36f24f147bc0d382aab514e
louis <louis.milhaud@universite-paris-saclay.fr>
2023-06-02 17:27:57 +0200

    end of day

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(int, char **) {
             << inv.second << std::endl;
 
   InvolutionsVector invST = janvierInsertion(p1);
-  std::cout << "Voici ses SYFT:" << std::endl;
+  std::cout << "Voici ses YFST:" << std::endl;
   display_involution(invST.first);
   std::cout << std::endl;
   display_involution(invST.second);
@@ -27,5 +27,23 @@ int main(int, char **) {
 
   std::cout << "------------------------------------------------------"
             << std::endl;
+
+  std::array<unsigned char, 8> t = {7, 3, 1, 5, 8, 4, 6, 2};
+  Permutation<8> p2 = Permutation<8>(t);
+  std::cout << "Voici une deuxieme permutation: " << p2 << std::endl;
+  std::pair<InvolutionNode *, InvolutionNode *> inv2 = robyInsertionCList(p2);
+  std::cout << "Voici ses InvolutionsCLists:" << std::endl
+            << inv2.first << std::endl
+            << inv2.second << std::endl;
+
+  InvolutionsVector invST2 = janvierInsertion(p2);
+  std::cout << "Voici ses YFST:" << std::endl;
+  display_involution(invST2.first);
+  std::cout << std::endl;
+  display_involution(invST2.second);
+  std::cout << "\nEt finalement ses chemins dans le graphe de Young-Fibonacci:\n" << std::endl;
+  auto r2 = permutationToChains(p2);
+  display_chains(r2);
+
   return 0;
 }
