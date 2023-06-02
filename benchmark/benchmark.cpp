d1cf417be0adfcf1645de87e83d158ac71fef506
louis <louis.milhaud@universite-paris-saclay.fr>
2023-06-02 13:22:37 +0200

    benchmark setup done

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -4,11 +4,10 @@
 #include "../src/involutionChainList.cpp"
 #include <benchmark/benchmark.h>
 
-static void BM_createPermutation(benchmark::State &state) {
-  std::array<unsigned char, 7> tab = {2, 7, 1, 5, 6, 4, 3};
-  Permutation<7> p1 = Permutation<7>(tab);
+static void BM_createPermutation(benchmark::State& state) {
+  for (auto _ : state)
+    Permutation<7> p1 = Permutation<7>({2, 7, 1, 5, 6, 4, 3});
 }
-
 BENCHMARK(BM_createPermutation);
 
 BENCHMARK_MAIN();
